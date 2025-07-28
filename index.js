cd0984e253b909c447831737904809932de9bc8a
Aml0n <chrgermain1@gmail.com>
2025-07-27 18:30:41 -0700

    logic(object): timer is now an object

--- index.js
+++ index.js
@@ -1,54 +1,56 @@
 const timer = document.querySelector(".timer");
-
-let seconds = 30;
-let minutes = 0;
-
-let paddedSeconds;
-let paddedMinutes;
-
-const interval = 1000;
-
-function timerDone() {
-    if (seconds === 0 && minutes === 0) {
-        return true
-    } else {
-        return false
-    }
-}
-
-function updateTimer() {
-
-    paddedSeconds = String(seconds).padStart(2, "0");
-    if (minutes < 10) {
-        paddedMinutes = String(minutes).padStart(2, "0")
-    } else {
-        paddedMinutes = String(minutes);
-    }
-    
-
-    timer.textContent = `${paddedMinutes}:${paddedSeconds}`
-
-    if (seconds === 0 && minutes !== 0) {
-        minutes--;
-        seconds = 59;
-    } else {
-        seconds--;
-    }
-
-    
-    if (timerDone()) {
-        setTimeout(() => {timer.textContent = "00:00"}, 1000)
-        return
-    } else {
-        setTimeout(updateTimer, interval);
-    }
-}
+const intervalMs = 1000;
 
 
-updateTimer();
+function Timer(startingTimeSeconds, startingTimeMinutes) {
+    this.startingTimeSeconds = startingTimeSeconds;
+    this.startingTimeMinutes = startingTimeMinutes;
 
-// setInterval(updateTimer, interval);
 
-// while (true) {
+    this.elapsedTimeSeconds = this.startingTimeSeconds;
+    this.elapsedTimeMinutes = this.startingTimeMinutes;
 
-// }
+    this.updateTimer = function() {
+
+        let paddedMinutes;
+        let paddedSeconds = String(this.elapsedTimeSeconds).padStart(2, "0");
+        if (this.elapsedTimeMinutes < 10) {
+            paddedMinutes = String(this.elapsedTimeMinutes).padStart(2, "0")
+        } else {
+            paddedMinutes = String(this.elapsedTimeMinutes);
+        }
+
+        timer.textContent = `${paddedMinutes}:${paddedSeconds}`
+
+        if (this.elapsedTimeSeconds === 0 && this.elapsedTimeMinutes !== 0) {
+            this.elapsedTimeMinutes--;
+            this.elapsedTimeSeconds = 59;
+        } else {
+            this.elapsedTimeSeconds--;
+        }
+
+        if (!(() => this.timerDone)) {
+            setTimeout(() => {timer.textContent = "00:00"}, 1000)
+            // console.log("hi");
+            // console.log(`${this.elapsedTimeSeconds}, ${this.elapsedTimeMinutes}`)
+            return
+        } else {
+            setTimeout(() => this.updateTimer(), intervalMs);
+            // setTimeout(this.updateTimer, intervalMs)
+            console.log(`${this.elapsedTimeSeconds}, ${this.elapsedTimeMinutes}`)
+        }
+    };
+
+    this.timerDone = function() {
+        if (this.elapsedTimeSeconds === 0 && this.elapsedTimeMinutes === 0) {
+            return true
+        } else {
+            return false
+        }
+    };
+
+};
+
+let timerOne = new Timer(0, 1)
+
+timerOne.updateTimer()
